444a9f5dd7428a13e9bf9d8200588f8d31fb6733
Marat Solncev <marat-solncev@mail.ru>
2016-10-22 21:24:13 +0300

    added project structure and all entities

---
+++ clinic/src/ru/itis/kpfu/group11501/solncev/entities/Appointment.java
@@ -0,0 +1,70 @@
+package ru.itis.kpfu.group11501.solncev.entities;
+
+/**
+ * Created by Марат on 01.11.2016.
+ */
+public class Appointment {
+    private long id;
+    private long client_id;
+    private long doctor_id;
+    private String date;
+    private String time;
+
+    public Appointment(long doctor_id, long client_id, String date, String time) {
+        this.doctor_id = doctor_id;
+        this.client_id = client_id;
+        this.date = date;
+        this.time = time;
+    }
+
+    public Appointment(long id, long client_id, long doctor_id, String date, String time) {
+        this.id = id;
+        this.client_id = client_id;
+        this.doctor_id = doctor_id;
+        this.date = date;
+        this.time = time;
+    }
+
+
+    public long getClient_id() {
+        return client_id;
+    }
+
+    public void setClient_id(long client_id) {
+        this.client_id = client_id;
+    }
+
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getDoctor_id() {
+        return doctor_id;
+    }
+
+    public void setDoctor_id(long doctor_id) {
+        this.doctor_id = doctor_id;
+    }
+
+}
---
+++ clinic/src/ru/itis/kpfu/group11501/solncev/entities/News.java
@@ -0,0 +1,70 @@
+package ru.itis.kpfu.group11501.solncev.entities;
+
+/**
+ * Created by Марат on 01.11.2016.
+ */
+public class News {
+    private long id;
+    private String photo;
+    private String topic;
+    private String text;
+    private String date;
+    private String time;
+
+    public News(long id, String photo, String topic, String text, String date, String time) {
+        this.id = id;
+        this.photo = photo;
+        this.topic = topic;
+        this.text = text;
+        this.date = date;
+        this.time = time;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
